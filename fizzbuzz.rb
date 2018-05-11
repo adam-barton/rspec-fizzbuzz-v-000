9c85dfb9a665ef3188ddb8e546b383874094b1c8
Adam Barton <adam.barton333@gmail.com>
2018-05-11 17:12:32 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,5 @@
 def fizzbuzz(int)
+<<<<<<< HEAD
     if (int) % 15 == 0 
  return "FizzBuzz"
  end
@@ -7,5 +8,16 @@ def fizzbuzz(int)
  end
  if (int) % 5 == 0 
   return "Buzz"
+=======
+  
+  if (int) % 3 == 0
+   "Fizz"
+ end
+ if (int) % 5 == 0 
+   "Buzz"
+ end
+  if (int) % 15 == 0 
+ "FizzBuzz"
+>>>>>>> 9b2cc7396e2881877827c2c358d81a4efddef806
  end
 end
